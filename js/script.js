3fdb1bfb96a4d6a1424729dbe54d2d4a2918003c
Valentin <choval@orange.fr>
2022-08-22 21:42:30 +0200

    first commit

--- js/script.js
+++ js/script.js
@@ -1,12 +1,16 @@
 let compt=0;
 let exemple = document.getElementById("boutonmenu");
-let text = document.getElementById("sous_menu_deroulant");
-text.classList.add("none");
+let texte = document.getElementsByClassName("sous_menu_deroulant");
 function Solution(){
-  text.classList.remove("none");
+  console.log("test")
+  for(i=0; i<texte.length;i++){
+    texte[i].classList.remove("none")
+  }
   compt+=1
   if (compt==2){
       compt=0;
-      text.classList.add("none");
+      for(i=0; i<texte.length;i++){
+        texte[i].classList.add("none")
+      }
   }
 }
